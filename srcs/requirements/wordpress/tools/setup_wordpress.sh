e95c52234750a35d12b5aa2f77ef47d7362c53a2
Rtijani <132397145+Rtijani@users.noreply.github.com>
2025-02-05 16:52:23 +0100

    Update setup_wordpress.sh

--- srcs/requirements/wordpress/tools/setup_wordpress.sh
+++ srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -7,32 +7,11 @@ sleep 5
     --admin_password="$WP_ADMIN_PASSWORD" \
     --admin_email="$WP_ADMIN_EMAIL" \
     --skip-email \
-    --path="$WP_PATH" \ # Use the variable
+    --path="$WP_PATH" \
     --allow-root
 
 /usr/local/bin/wp-cli.phar user create "$WP_USER" "$WP_USER_EMAIL" \
     --user_pass="$WP_USER_PASSWORD" \
     --role="$WP_ROLE" \
-    --path="$WP_PATH" \ # Use the variable
+    --path="$WP_PATH" \
     --allow-root
-
-
-#service php7.4-fpm start
-#service php7.4-fpm stop
-/usr/sbin/php-fpm7.4 -F
-
-#--url="$WP_URL": Sets the WordPress site URL.
-#--title="$WP_TITLE": Sets the site title.
-#--admin_user="$WP_ADMIN_USER": Sets the admin username.
-#--admin_password="$WP_ADMIN_PASSWORD": Sets the admin password.
-#--admin_email="$WP_ADMIN_EMAIL": Sets the admin email.
-#--skip-email: Skips sending email notifications during installation.
-#--path='/var/www/wordpress': Specifies the installation directory path.
-#--allow-root: Allows running WP-CLI commands as the root user.
-
-#"$WP_USER": User's login (username).
-#"$WP_USER_EMAIL": User's email address.
-#--user_pass="$WP_USER_PASSWORD": User's password.
-#--role="$WP_ROLE": Sets the user role to "author."
-#--path='/var/www/wordpress': Specifies the installation directory path.
-#--allow-root: Allows running WP-CLI commands as the root user.
